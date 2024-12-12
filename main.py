43be2bfb5c68799dc0683b70850c6bdbf3ac8539
Seksan Sukkasem <153727319+65160410@users.noreply.github.com>
2024-12-12 13:49:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ from contextlib import asynccontextmanager
 app = FastAPI()
 
 # ข้อมูล token และ channel secret สำหรับ LINE
-ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN", "O0Vi8xE7Wh3A6BahSUC6O0VKR7RxR0p27jHBl1h39OdH9/d3cEtmrS4QT91BUEDmmrRqLrUiKLVxlJcggXWQ/MwNBJttPBjKEw8Oifg9O06on+Ab3UzbvQ7E8W56z5GeOIHvROzUsRVagavLPiTIbwdB04t89/1O/w1cDnyilFU=")
-CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", "701c99f9fa1b4d0261e6f4dedcce76c8")
+ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN", "R/lngg68QycO4XcvPvlsbciqvWUp31SsBCWKkWOeAFJiGvc744eoRMHiT7IPkHTqT0aosHH1ChmRMQPI00l0dsirnCtoLzdIixcw8fpx+0xUxVDSfYickIWpAVVtvdvEOPpwho2/IW6PsRwkbT5HswdB04t89/1O/w1cDnyilFU=")
+CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", "aa843036e806379620e61f24153850df")
 
 # ข้อมูล Gemini api key
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBrn9N8g0RnZrYhW-vFe3Tb2ytibKcsU3E")
@@ -239,12 +239,12 @@ gemini = GeminiRAGSystem(
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # ข้อมูลตัวอย่างที่ใช้สำหรับ Gemini
-    sample_documents = [
-        "จิรภัทร ทำดี คือ ชายหนุ่มที่มีความสามารถในการเขียนโปรแกรมที่มาจากบริษัท ClickNext ที่เป็นบริษัทด้านการพัฒนาโปรแกรมที่มีชื่อเสียง",
-        "จิรภัทร ทำดี มีชื่อเล่นว่า ปาล์ม เกิดวันที่ 25 มกราคม 2555 ที่จังหวัดสระบุรี ศึกษาจบจากมหาวิทยาลัยบูรพา สาขาวิศวกรรมคอมพิวเตอร์",
-        "งาน BUU-Bootcamp-2024 จัดที่มหาวิทยาลัยบูรพา ในวันที่ 25 มกราคม ปีค.ศ.2024 โดยมีการจัดกิจกรรมต่าง ๆ ที่เกี่ยวข้องกับการพัฒนาซอฟต์แวร์ เวลา 9:00 น. - 16:00 น.",
-        "มหาวิทยาลัยบูรพา สาขาวิชาAI ปีการศึกษา 2565 มีนักศึกษาจำนวน 100 คน มีอาจารย์ที่ปรึกษา 10 คน"
-    ]
+sample_documents = [
+    "เสกสันต์เป็นชายหนุ่มที่มีความสามารถในการเขียนโปรแกรม เขาทำงานที่บริษัท ClickNext ซึ่งเป็นบริษัทพัฒนาโปรแกรมที่มีชื่อเสียง",
+    "เสกสันต์มีชื่อเล่นว่ากั้ง เกิดวันที่ 19 มกราคม พ.ศ. 2546 ที่จังหวัดสระบุรี เขายังไม่สำเร็จการศึกษาจากมหาวิทยาลัยบูรพา สาขาวิชา AAI",
+    "งาน BUU-Bootcamp-2024 จัดขึ้นที่มหาวิทยาลัยบูรพาในวันที่ 25 มกราคม ค.ศ. 2024 โดยมีกิจกรรมที่เกี่ยวข้องกับการพัฒนาซอฟต์แวร์ตั้งแต่เวลา 9:00 น. ถึง 16:00 น.",
+    "ในปีการศึกษา 2565 สาขาวิชา AI มหาวิทยาลัยบูรพามีนักศึกษาจำนวน 100 คน และมีอาจารย์ที่ปรึกษาจำนวน 10 คน"
+]
     
     # เพิ่มข้อมูลตัวอย่างลงใน Gemini
     for doc in sample_documents:
